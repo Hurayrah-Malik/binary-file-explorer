58435d8a6e1a70d2476d6157bb7752f48b31a271
Hurayrah <hurayrah92@gmail.com>
2026-02-23 02:41:49 -0500

    finished hex editor

--- explorer.py
+++ explorer.py
@@ -27,34 +27,65 @@ def main():
 def hexdump(raw_bytes: bytes):
     # list of decimal bytes
     bytes_decimal = list(raw_bytes)
+
     # convert all these decimals to hex
     bytes_hex = []
     for byte in bytes_decimal:
-        bytes_hex.append(hex(byte))
+        bytes_hex.append(f"{byte:02x}")
 
-    # print first line
-    print(f"{hex(16)}  {bytes_hex[:16]}")
-    printable_bytes(bytes_decimal)
+    # create the column generators
+    column1_gen = get_offset()
+    column2_gen = get_hex_bytes(bytes_hex)
+    column3_gen = printable_bytes(bytes_decimal)
+
+    # print the first line for now (while loop later)
+    for x in range(len(bytes_decimal) // 16):
+        column1 = next(column1_gen)
+        column2 = next(column2_gen)
+        column3 = next(column3_gen)
+        print(f"{column1}  {column2}  |{column3}|")
 
 
+# COLUMN 1
+# returns the next offset number
+def get_offset():
+    offset = 0
+    while True:
+        yield f"{offset:08x}"
+        offset += 16
+
+
+# COLUMN 2
+# return the next row of bytes in hex
+def get_hex_bytes(bytes_hex: list):
+    start = 0
+    end = 16
+    while True:
+        yield " ".join(bytes_hex[start:end])
+        start += 16
+        end += 16
+
+
+# COLUMN 3
 # function for building column 3 (each byte shown as ascii character if printable)
 # printable: if byte is between 32 and 126, print letter
 # not printable: print '.'
-# print the bytes one by one first (test)
 def printable_bytes(bytes_decimal: list):
-    counter = 0
-    # loop over every byte (decimal list)
-    for byte in bytes_decimal:
-        # if printable
-        if 32 <= byte <= 126:
-            print(chr(byte), end="")
-            counter += 1
-        # if not printable
-        else:
-            print(".", end="")
-            counter += 1
-        if counter > 16:
-            break
+    start = 0
+    end = 16
+    result = ""
+
+    while True:
+        bytes_to_print = bytes_decimal[start:end]
+        for byte in bytes_to_print:
+            if 32 <= byte <= 126:
+                result += chr(byte)
+            else:
+                result += "."
+        yield result
+        result = ""
+        start += 16
+        end += 16
 
 
 if __name__ == "__main__":
